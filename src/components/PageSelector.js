2a4e1efd5a507abb73713067c556af6f1190d3a3
Nick Allen <nwestallen@gmail.com>
2021-01-22 14:47:22 -0500

    improved page selector format

--- src/components/PageSelector.js
+++ src/components/PageSelector.js
@@ -5,6 +5,18 @@ import styled from 'styled-components';
 const PageSelector = (props) => {
     const { nextPage, prevPage, page} = props;
 
+    const Selector = styled.div`
+        width: 100%;
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: center;
+
+        h2 {
+            color: #2E382E;
+            text-shadow: .8px .8px #50C9CE;
+        }
+    `;
+
     const PageButton = styled.button`
         color: white;
         background-color: #9883E5;
@@ -14,11 +26,11 @@ const PageSelector = (props) => {
     `;
 
     return(
-        <div>
+        <Selector>
             <PageButton onClick={prevPage}>Previous</PageButton>
             <h2>Page {page}</h2>
             <PageButton onClick={nextPage}>Next</PageButton>
-        </div>
+        </Selector>
     )
 
 }
