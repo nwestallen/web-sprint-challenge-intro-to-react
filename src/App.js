3d6fc7b2ff7d4b29313cb30e3967ab7bdbca6d9b
Nick Allen <nwestallen@gmail.com>
2021-01-22 11:52:11 -0500

    added Character component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import styled from 'styled-components'
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,19 +11,19 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [name, setName] = useState('Rick');
+  const [charData, setCharData] = useState([]);
 
   useEffect( () => {
-    console.log('Side Effect');
     axios
     .get(`https://rickandmortyapi.com/api/character`)
-    .then(res => console.log(res))
+    .then(res => setCharData(res.data))
     .catch(err => console.log(err));
-  });
+  },[]);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Character = (props) => {
+
+    const { name, setName } = props;
+
+    const Char = styled.div`
+        background-color: grey;
+    `;
+
+    return(
+        <Char>
+            <h2>It's RICK</h2>
+        </Char>
+    )
+}
+
+export default Character
